d64f0392be36a37a33aeafa440e549275c4d5b41
Schwarzercm <schwarzercm@gmail.com>
2020-10-29 13:05:05 +0000

    addded the ability to sign up account and add it to the mongodb

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_login import current_user, login_user, login_required, logout_user, LoginManager
 from flask_socketio import SocketIO, join_room, leave_room
-
-from db import get_user
+from pymongo.errors import DuplicateKeyError
+from db import get_user, save_user
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -34,6 +34,23 @@ def login():
             message = 'failed to login'
     return render_template('login.html', message=message)
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('menu'))
+
+    message = ''
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        try: 
+            save_user(username, email, password)
+            return redirect(url_for('login')) 
+        except DuplicateKeyError:
+            message = 'User already exists!'
+    return render_template('signup.html', message=message)
+    
 
 @app.route("/logout")
 @login_required
